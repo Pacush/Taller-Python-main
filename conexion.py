d5e76fe7ba6b8f77fa44cb79cd95a5e52082c520
José Sánchez <josealejandro.sancheznaranjo@gmail.com>
2025-03-21 21:51:09 -0600

    port selection

--- conexion.py
+++ conexion.py
@@ -1,17 +1,20 @@
 import mysql.connector
 
+
 class conexion:
     def __init__(self):
         self.user="root"
         self.password=""
         self.database="dbtaller_mecanico"
         self.host="localhost"
+        self.port="3307"
 
     def open(self):
         self.conn=mysql.connector.connect(host=self.host,
                                           user=self.user,
                                           passwd=self.password,
-                                          database=self.database)
+                                          database=self.database,
+                                          port=self.port)
         return self.conn
 
     def close(self):
